f00d63029fafae74dd8ad9ec5236e2594dedd9ad
purudhingra <dhingrpu@sheridancollege.ca>
2020-08-06 16:25:02 -0400

    Modification

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -8,45 +8,34 @@ package ca.sheridancollege.project;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  * @author dancye, 2018
- * @modifier Harleen Kaur Grewal, 17 July 2020
+ * @modifier Vikas Rai, 6 August 2020
  */
-public class Player 
+public class Player
 {
     private String playerID; //the unique ID for this player
     
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
     public Player(String name)
     {
-        playerID= name;
+        setPlayerID(name);
     }
     
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
+    public String getPlayerID()
     {
         return playerID;
     }
-
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
+    
+    public void setPlayerID(String givenID)
     {
-        playerID = givenID;
+        if (givenID.equals(""))
+            throw new IllegalArgumentException("Invalid Name. Game Over");
+        else
+            playerID = givenID;
+    }
+    
+    public void check(Player player)
+    {
+        if (playerID.equals(player.playerID))
+            throw new IllegalArgumentException("Players should have different names. Game Over");
     }
 
-	/**
-	 * 
-	 * @param player
-	 */
-	public void check(Player player) {
-		// TODO - implement Player.check
-		throw new UnsupportedOperationException();
-	}
-    
 }
